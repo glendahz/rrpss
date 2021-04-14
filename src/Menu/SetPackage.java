9562cf875e6e108af521e483bd7a093be9118418
craigalix <krajidon@gmail.com>
2021-04-14 21:44:30 +0800

    JavaDocs and editMenuItem added

--- src/Menu/SetPackage.java
+++ src/Menu/SetPackage.java
@@ -2,12 +2,24 @@ package Menu;
 
 import java.util.ArrayList;
 
+/**
+ * Represents 1 Set menu item object.
+ * Attributes = int SetIndex, ArrayList<String> setList, double Price.
+ * @author Craigdon Lee
+ * @version 1.0
+ *
+ */
 public class SetPackage {
 	
 	ArrayList<String> setList = new ArrayList<String>();
 	int setIndex;
 	double setPrice;
 	
+	/**
+	 * @param SetIndex		Sets the index of a specific setpackage
+	 * @param setList		Sets the array list of menu items
+	 * @param Price			Sets the price of the Set menu item.
+	 */
 	public SetPackage(int SetIndex, ArrayList<String> setList, double Price)
 	{
 		this.setList = setList;
@@ -15,18 +27,30 @@ public class SetPackage {
 		this.setPrice = Price;
 	}
 
+	/**
+	 * @return item type of set menu
+	 */
 	public String getItemType() {
 		return "setMenu";
 	}
 	
+	/**
+	 * @return returns the index that represents the specific set menu item
+	 */
 	public int getIndex() {
 		return this.setIndex;
 	}
 	
+	/**
+	 * @return Price of specific set menu item
+	 */
 	public double getPrice() {
 		return this.setPrice;
 	}
 	
+	/**
+	 * @return Array list of menu items.
+	 */
 	public ArrayList<String> getSetList() {
 		return this.setList;
 	}
